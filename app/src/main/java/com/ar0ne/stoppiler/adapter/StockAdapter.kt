dcb25fab417ecf2b009ecde697aff4f04c245841
ar0ne <camtbitakou@yandex.ru>
2020-03-30 00:06:06 +0300

    update ui

--- app/src/main/java/com/ar0ne/stoppiler/adapter/StockAdapter.kt
+++ app/src/main/java/com/ar0ne/stoppiler/adapter/StockAdapter.kt
@@ -9,6 +9,7 @@ import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.ar0ne.stoppiler.R
+import com.ar0ne.stoppiler.domain.GoodsType
 import com.ar0ne.stoppiler.domain.Stock
 import com.ar0ne.stoppiler.domain.StockRecord
 import com.ar0ne.stoppiler.ui.SolidFontAwesomeTextView
@@ -31,20 +32,45 @@ class StockAdapter(var stock: Stock, val callback: Callback, val removeCallback:
 
     inner class MainHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
         private val productName = itemView.findViewById<TextView>(R.id.product_name)
-        private val productPhoto = itemView.findViewById<ImageView>(R.id.product_photo)
+        private val productPhoto =
+            itemView.findViewById<SolidFontAwesomeTextView>(R.id.product_photo)
         private val productVolume = itemView.findViewById<TextView>(R.id.product_volume)
-        private val btnRemove = itemView.findViewById<SolidFontAwesomeTextView>(R.id.product_btn_remove)
+        private val btnRemove =
+            itemView.findViewById<SolidFontAwesomeTextView>(R.id.product_btn_remove)
+
         fun bind(record: StockRecord) {
             productName.text = record.goods.name
             productVolume.text = "${record.volume} ${record.goods.unit.repr}"
-            productPhoto.setImageResource(R.mipmap.ic_launcher_round)
+            productPhoto.text = getStockRecordIcon(itemView, record)
 
             itemView.setOnClickListener {
-                if (adapterPosition != RecyclerView.NO_POSITION) callback.onItemClicked(stock.getRecord(adapterPosition))
+                if (adapterPosition != RecyclerView.NO_POSITION) callback.onItemClicked(
+                    stock.getRecord(
+                        adapterPosition
+                    )
+                )
             }
 
             btnRemove.setOnClickListener {
-                if (adapterPosition != RecyclerView.NO_POSITION) removeCallback.onItemClicked(stock.getRecord(adapterPosition))
+                if (adapterPosition != RecyclerView.NO_POSITION) removeCallback.onItemClicked(
+                    stock.getRecord(
+                        adapterPosition
+                    )
+                )
+            }
+        }
+    }
+
+    fun getStockRecordIcon(view: View, record: StockRecord): String {
+        when (record.goods.type) {
+            GoodsType.FOOD -> {
+                return view.resources.getString(R.string.food_icon)
+            }
+            GoodsType.WATER -> {
+                return view.resources.getString(R.string.water_icon)
+            }
+            GoodsType.TOILET_PAPER -> {
+                return view.resources.getString(R.string.toilet_paper_icon)
             }
         }
     }
